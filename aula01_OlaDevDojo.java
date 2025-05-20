2f63afe2f7414178823181722543a46192a5f9a3
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-20 16:14:12 -0300

    adiciona aulas 1 e 2

---
+++ aula01_OlaDevDojo.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
+public class aula01_OlaDevDojo {
+    //Isso é um comentario de uma linha
+    /*Comentário de múltiplas linhas
+    */
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
