4cc50ed8430aa0c1ecbd77b0b539b99e6a2be34a
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-20 18:22:13 -0300

    adiciona aula 2 tipos primitivos

--- Aula02TiposPrimitivos.java
+++ Aula02TiposPrimitivos.java
@@ -4,6 +4,19 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         //int, double, float, char, byte, short, long, boolean
         int idade = 10;
+        long numeroGrande = (long) 155.23;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = (float)2500.0D;
+        byte idadeByte = -121;
+        short idadeShort = 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+        String nome = "Goku";
+
         System.out.println("A idade é " +idade+ " anos");
+        System.out.println(verdadeiro);
+        System.out.println("char "+caractere);
+        System.out.println("Oi meu nome é : " +nome);
     }
 }
