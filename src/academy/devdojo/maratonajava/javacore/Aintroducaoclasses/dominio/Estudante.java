f1c15d5d06cd147b6c7f0463dae82747726114d9
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-23 14:22:33 -0300

    adiciona aula Estudante parte 01

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Estudante.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Estudante {
+
+    public String nome;
+    public String idade;
+    public String sexo;
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/teste/EstudanteTeste02.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Aintroducaoclasses.teste;
+
+public class EstudanteTeste02 {
+    public static void main(String[] args) {
+
+    }
+}
