439d5bb95eeb641865dc9d28c334afeea2e8b927
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-23 17:54:01 -0300

    adiciona aula sobre metodos parte 03

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/teste/CalculadoraTeste03.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/teste/CalculadoraTeste03.java
@@ -5,7 +5,10 @@ import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.
 public class CalculadoraTeste03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        double result = calculadora.divideDoisNumeros(20, 2);
+        double result = calculadora.divideDoisNumeros(20, 0);
         System.out.println(result);
+        System.out.println(calculadora.divideDoisNumeros02(20, 0));
+        System.out.println("----------------------");
+        calculadora.imprimeDivisaoDeDoisNumeros(86, 0);
     }
 }
