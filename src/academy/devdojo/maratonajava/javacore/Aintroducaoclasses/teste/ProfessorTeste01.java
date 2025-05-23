e8cfe82cb89c0df82e1ce59eec60b0cd3542bd49
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-23 15:03:40 -0300

    adiciona aula com título professor parte 01

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/teste/ProfessorTeste01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Aintroducaoclasses.teste;
+
+import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Professor;
+
+public class ProfessorTeste01 {
+    public static void main(String[] args) {
+        Professor professor = new Professor();
+        professor.nome = "Bruno";
+        professor.idade = 39;
+        professor.sexo = 'M';
+
+        System.out.println(" Nome " +professor.nome + " idade "+professor.idade + " sexo "+professor.sexo);
+
+    }
+}
