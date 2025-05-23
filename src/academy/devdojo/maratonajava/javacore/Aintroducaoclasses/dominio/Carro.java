1ae079d1b8a1ad3d564154e66daf3eb52ae74c9f
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-23 16:30:07 -0300

    adiciona aula com titulo carro exercicio continuação

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Carro.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Carro {
+    public String nome;
+    public String modelo;
+    public int ano;
+}
+
