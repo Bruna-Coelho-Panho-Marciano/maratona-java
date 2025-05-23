c7c997b56cad742fb4838cfc0576b4aff63f2c1c
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-23 16:34:19 -0300

    adiciona classe professor

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Professor.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Professor {
+    public String nome = "";
+    public int idade ;
+    public char sexo;
+}
