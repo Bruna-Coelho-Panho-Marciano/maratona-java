aab9affe9d23c007d764cf4ed7b47e173ceaf635
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-26 10:59:03 -0300

    adiciona aula sobre parametros tipo referencia continuação

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Estudante {
+    public String nome ;
+    public int idade;
+    public char sexo;
+}
