ca6d7c408f512a0b97fd0556612525a9e0036c7d
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-26 10:47:24 -0300

    adiciona aula sobre parametros tipo referencia parte 01

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/teste/EstudanteTeste01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Bintroducaometodos.teste;
+
+import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTeste01 {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
+
+        estudante01.nome = "Bruno";
+        estudante01.idade = 39;
+        estudante01.sexo = 'M' ;
+
+        estudante02.nome = "Bruna";
+        estudante02.idade = 31;
+        estudante02.sexo = 'F' ;
+
+        impressora.imprime(estudante01);
+
+        impressora.imprime(estudante02);
+
+    }
+}
