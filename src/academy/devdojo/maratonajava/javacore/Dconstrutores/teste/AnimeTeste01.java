99b68bf21201ac24a1ea0bde2db1307959e96395
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-27 14:46:29 -0300

    adiciona aula construtores continuação

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/teste/AnimeTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/teste/AnimeTeste01.java
@@ -4,9 +4,7 @@ import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.
 
 public class AnimeTeste01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Bruno", "TV", 12, "Ação");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("Bruno", "TV", 12, "Ação", "Production IG");
         anime.imprime();
-        anime2.imprime();
     }
 }
