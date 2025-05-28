1bd9db72b66d5f81bbac6f96aa8e827d90dd9573
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-28 15:31:08 -0300

    adiciona aula sobre herança

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/teste/HerancaTeste01.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Hheranca.teste;
+
+import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTeste01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("012345-209");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Bruno");
+        pessoa.setCpf("1111111111");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Elis");
+        funcionario.setCpf("2222222");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+        System.out.println("-------------");
+        funcionario.imprime();
+    }
+}
