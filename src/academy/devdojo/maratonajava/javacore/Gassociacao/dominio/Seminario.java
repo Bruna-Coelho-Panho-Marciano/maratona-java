0e41cbf005447f2bd5b1289a0ee25b88378f10f3
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-28 15:03:26 -0300

    adiciona aula associação exercicios continuação

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Seminario.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Seminario.java
@@ -5,13 +5,9 @@ public class Seminario {
     private  Aluno[] alunos;
     private Local local;
 
-    public Seminario(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public Seminario(String titulo, Aluno[] alunos) {
-        this.titulo = titulo;
-        this.alunos = alunos;
+    public Seminario(String titulo, Local local) {
+       this.titulo = titulo;
+       this.local = local;
     }
 
     public Seminario(String titulo, Aluno[] alunos, Local local) {
@@ -20,27 +16,15 @@ public class Seminario {
         this.local = local;
     }
 
-    public Local getLocal() {
-        return local;
-    }
+    public Local getLocal() {return local;}
 
-    public void setLocal(Local local) {
-        this.local = local;
-    }
+    public void setLocal(Local local) {this.local = local;}
 
-    public Aluno[] getAlunos() {
-        return alunos;
-    }
+    public Aluno[] getAlunos() {return alunos;}
 
-    public void setAlunos(Aluno[] alunos) {
-        this.alunos = alunos;
-    }
+    public void setAlunos(Aluno[] alunos) {this.alunos = alunos;}
 
-    public String getTitulo() {
-        return titulo;
-    }
+    public String getTitulo() {return titulo;}
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+    public void setTitulo(String titulo) {this.titulo = titulo;}
 }
