495aa315742219cd0ff1bc2c15cbf11a797dac53
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-29 14:56:31 -0300

    adiciona aula sobre enumeração: sobrescrita de métodos

--- src/academy/devdojo/maratonajava/javacore/Kenum/teste/ClienteTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/teste/ClienteTeste01.java
@@ -1,19 +1,23 @@
 package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Kenum.teste;
 
 import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
-
 import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
+import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoPagamento;
 
 
 public class ClienteTeste01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Elis", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
-        Cliente cliente2 = new Cliente("Luisa", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente1 = new Cliente("Elis", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Luisa", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+
 
     }
+
 }
 
 
