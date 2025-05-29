4b73824e2c4e04e9d3d179b8f312df19adf13458
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-29 16:57:34 -0300

    adiciona aula sobre implementando múltiplas interfaces

---
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataRemover.java
@@ -0,0 +1,5 @@
+package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
+
+public interface DataRemover {
+    public abstract void remove();
+}
