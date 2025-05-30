3e8571f95a3600ed46ae1b28e42a1b194c9c944f
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-30 10:51:08 -0300

    adiciona aula sobre parametros polimórficos continuação

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/teste/ProdutoTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/teste/ProdutoTeste01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Npolimorfismo.teste;
 
 import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,9 +9,11 @@ public class ProdutoTeste01 {
     public static void main(String[] args) {
         Computador computador = new Computador(" NUC10i7", 11000);
         Tomate tomate = new Tomate(" Italiano",6);
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        Televisao televisao = new Televisao("Samsung 50\" ",5000);
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("----------------------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
-
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("----------------------------");
+        CalculadoraImposto.calcularImposto(televisao);
     }
 }
