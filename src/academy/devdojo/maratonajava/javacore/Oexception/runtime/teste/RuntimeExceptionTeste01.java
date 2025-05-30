42ff69f55860b950583a9113c8c1fdd51afe2927
bruna.marciano <brunacpmarciano@gmail.com>
2025-05-30 13:43:51 -0300

    adiciona aula sobre RuntimeException

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/teste/RuntimeExceptionTeste01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Oexception.runtime.teste;
+
+public class RuntimeExceptionTeste01 {
+    public static void main(String[] args) {
+        // Checked e Unchecked
+        int[] nums = {1,2};
+        System.out.println(nums[2]);
+    }
+}
