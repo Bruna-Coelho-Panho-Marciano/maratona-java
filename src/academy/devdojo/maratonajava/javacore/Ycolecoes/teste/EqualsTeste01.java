bc0910544d69f3376b58fba1a4b081d8046c3e91
bruna.marciano <brunacpmarciano@gmail.com>
2025-07-09 14:50:52 -0300

    adiciona aula sobre Equals parte 02

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/EqualsTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/EqualsTeste01.java
@@ -4,7 +4,7 @@ import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.
 public class EqualsTeste01 {
     public static void main(String[] args) {
         Smartphone s1 = new Smartphone("1ABC1","iPhone");
-        Smartphone s2 = new Smartphone("1ABC1","iPhone");
+        Smartphone s2 = new Smartphone("1ABC1","Pixel");
         System.out.println(s1.equals(s2));
     }
     
