a73d0cae142c10c42625150ee2e0e2a7afac6aee
bruna.marciano <brunacpmarciano@gmail.com>
2025-07-10 17:34:29 -0300

    adicionar aula sobre Iterator

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/IteratorTeste01.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Ycolecoes.teste;
+
+import academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorTeste01 {
+    public static void main(String[] args) {
+        List<Manga> mangas = new ArrayList<>(6);
+        mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9, 0));
+        mangas.add(new Manga(1L, "Berseck", 9.5, 5));
+        mangas.add(new Manga(4L, "Pokemon", 3.2, 0));
+        mangas.add(new Manga(3L, "Attack on Titan", 11.20, 2));
+        mangas.add(new Manga(2L, "Dragon ball Z", 2.99, 0));
+
+//        Iterator<Manga> mangaIterator = mangas.iterator();
+//        while (mangaIterator.hasNext()) {
+//            if (mangaIterator.next().getQuantidade() == 0) {
+//                mangaIterator.remove();
+//            }
+//        }
+        mangas.removeIf(manga -> manga.getQuantidade() == 0);
+        System.out.println(mangas);
+
+    }
+}
+
+
+
+
