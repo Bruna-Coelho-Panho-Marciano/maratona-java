5df318a831cec7a8dfd89a585be8a733eff00597
bruna.marciano <brunacpmarciano@gmail.com>
2025-08-04 09:53:36 -0300

    adicionar aula sobre Encadeando chamadas,allOf e anyOf

---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/dominio/Quote.java
@@ -0,0 +1,48 @@
+package academy.devdojo.maratonajava.introducao.src.academy.devdojo.maratonajava.javacore.ZZGconcorrencia.dominio;
+
+// storeName:price:discountCode
+public final class Quote {
+    private final String store;
+    private final double price;
+    private final Discount.Code discountCode;
+
+    private Quote(String store, double price, Discount.Code discountCode) {
+        this.store = store;
+        this.price = price;
+        this.discountCode = discountCode;
+    }
+
+    /**
+     * Creates new Quote object from the value following the pattern storeName:price:discountCode
+     * @param value containing storeName:price:discountCode
+     * @return new Quote with values from @param value
+     */
+
+    public static Quote newQuote(String value){
+        String[] values = value.split(":");
+        String replace_value = values[1].replace(",",".");
+        return new Quote(values[0], Double.parseDouble(replace_value), Discount.Code.valueOf(values[2]));
+
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "store='" + store + '\'' +
+                ", price=" + price +
+                ", discountCode=" + discountCode +
+                '}';
+    }
+
+    public String getStore() {
+        return store;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Discount.Code getDiscountCode() {
+        return discountCode;
+    }
+}
